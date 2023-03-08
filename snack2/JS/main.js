baa4035091ba04a4d35f67d6f04457be4db2ed40
tinotringali <tringali.tino94@gmail.com>
2023-03-08 21:25:16 +0100

    creata costante lista studenti

---
+++ snack2/JS/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const students =
+[
+{ 
+    id: '213',
+    name: 'Marco della Rovere',
+    grades : '78'
+},
+{
+    id: '110',
+    name: 'Paola Cortellessa',
+    grades : '96'
+},
+{
+    id: '250',
+    name: 'Andrea Mantegna ',
+    grades : '48'
+ },
+ {
+    id: '145',
+    name: 'Gaia Borromini',
+    grades : '74'
+ },
+ {
+    id: '196',
+    name: 'Luigi Grimaldello',
+    grades : '68'
+},
+{
+    id: '102',
+    name: 'Piero della Francesca',
+    grades : '50'
+},
+{
+    id: '120',
+    name: 'Francesca da Polenta',
+    grades : '84'
+}
+]
