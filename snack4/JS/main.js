dc050b5e0a17510d8eb1a38d09b667f9782b9583
tinotringali <tringali.tino94@gmail.com>
2023-03-09 23:48:21 +0100

    lista squadre Super lega

---
+++ snack4/JS/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// lista squadre Super lega
+
+const superLega =
+[
+{ 
+    team: 'Juventus',
+    Pt : 0,
+    faults : 0
+},
+{
+    name: 'Barcellona',
+    Pt : 0,
+    faults : 0
+},
+{
+    name: 'Real Madrid',
+    Pt : 0,
+    faults : 0
+ },
+ {
+    name: 'PSG',
+    Pt : 0,
+    faults : 0
+ },
+ {
+    name: 'Manchester City',
+    Pt : 0,
+    faults : 0
+},
+{
+    name: 'Bayern Monaco',
+    Pt : 0,
+    faults : 0
+},
+]
