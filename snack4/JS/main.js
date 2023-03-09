8268b40dbaca33d8e8dabf1e5b466a8e03efcfb7
tinotringali <tringali.tino94@gmail.com>
2023-03-10 00:00:33 +0100

    punti e falli sqaudre

--- snack4/JS/main.js
+++ snack4/JS/main.js
@@ -11,31 +11,40 @@ const superLega =
 { 
     team: 'Juventus',
     Pt : 0,
-    faults : 0
+    falliSubiti : 0
 },
 {
     name: 'Barcellona',
     Pt : 0,
-    faults : 0
+    falliSubiti : 0
 },
 {
     name: 'Real Madrid',
     Pt : 0,
-    faults : 0
+    falliSubiti : 0
  },
  {
     name: 'PSG',
     Pt : 0,
-    faults : 0
+    falliSubiti : 0
  },
  {
     name: 'Manchester City',
     Pt : 0,
-    faults : 0
+    falliSubiti : 0
 },
 {
     name: 'Bayern Monaco',
     Pt : 0,
-    faults : 0
+    falliSubiti : 0
 },
 ]
+console.log(superLega)
+
+// punti e falli sqaudre
+const firstTeam = superLega.map(element => {
+    element["Pt"] = getRandomInt(80);
+    element ["falliSubiti"] = getRandomInt(150);
+    return (element)
+})
+console.log(firstTeam)
